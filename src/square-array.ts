e2003dea9d48fb4c8d53867059e670d7ae5f2c1b
FlavioMatias <flaviomatiasnf@gmail.com>
2025-10-05 21:59:44 -0300

    feat: implement square function using simple for

---
+++ src/square-array.ts
@@ -0,0 +1,8 @@
+
+function SquareSimpleFor(arr: number[]): number[] {
+    const result : number[] = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(arr[i]! ** 2);
+    }
+    return result;
+}
